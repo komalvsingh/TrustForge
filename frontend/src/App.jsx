435541323f5ee489cc527e9375d08323337a93c8
AaryanKalbhor <aaryankalbhor13@gmail.com>
2026-02-06 11:40:25 +0530

    chore: ignore hardhat artifacts and cache

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,26 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { BlockchainProvider } from "./context/BlockchainContext";
+import { DAOProvider } from "./context/DAOContext";
 
 import Home from "./pages/home";
 import Dashboard from "./pages/Dashboard";
 import Lend from "./pages/Lend";
 import BorrowerDashboard from "./pages/borrower/BorrowerDashboard";
+import DAO from "./pages/DAO";
 function App() {
   return (
     <BlockchainProvider>
+      <DAOProvider>
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dao" element={<DAO />} />
           <Route path="/lend" element={<Lend />} />
           <Route path="/borrow" element={< BorrowerDashboard />} />
         </Routes>
       </Router>
+      </DAOProvider>
     </BlockchainProvider>
   );
 }
