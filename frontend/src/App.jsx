a26053d7b231b099c72bd8e86b33fdb47edcf956
komalvsingh <komalvsingh1111@gmail.com>
2026-02-06 11:58:14 +0530

    dao added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { BlockchainProvider } from "./context/BlockchainContext";
 import { DAOProvider } from "./context/DAOContext";
+import DAO from "./pages/DAO"
 
 import Home from "./pages/home";
 import Dashboard from "./pages/Dashboard";
